8f7c530c520d4767683a2c1195289937692360c3
nazarli-shabnam <shabnamnezerli@gmail.com>
2026-05-12 14:32:12 +0400

    feat(models): add SQLAlchemy ORM models

---
+++ apps/api/src/models/__init__.py
@@ -0,0 +1,5 @@
+from src.models.audit_log import AuditLog
+from src.models.installation import GitHubInstallation
+from src.models.job import Job
+
+__all__ = ["AuditLog", "GitHubInstallation", "Job"]
